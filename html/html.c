04c98c2bfc1c7a38e133c9a603f92ac555f81d83
Alex Kocharin <alex@kocharin.ru>
2012-07-10 21:28:16 +0400

    changing class name in blockcode to avoid collisions

--- html/html.c
+++ html/html.c
@@ -122,7 +122,7 @@ rndr_blockcode(struct buf *ob, const struct buf *text, const struct buf *lang, v
 
 	if (lang && lang->size) {
 		size_t i, cls;
-		BUFPUTSL(ob, "<pre><code class=\"");
+		BUFPUTSL(ob, "<pre><code class=\"lang-");
 
 		for (i = 0, cls = 0; i < lang->size; ++i, ++cls) {
 			while (i < lang->size && isspace(lang->data[i]))

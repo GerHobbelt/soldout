feeb0665672869079cdc334513bd7bb44eb39b52
fluffy <fluffy@beesbuzz.biz>
2020-03-25 16:33:28 -0700

    Add HTML5 block elements

--- src/html_blocks.c
+++ src/html_blocks.c
@@ -1,6 +1,6 @@
 /* ANSI-C code produced by gperf version 3.0.3 */
 /* Command-line: gperf -L ANSI-C -N hoedown_find_block_tag -c -C -E -S 1 --ignore-case -m100 html_block_names.gperf  */
-/* Computed positions: -k'1-2' */
+/* Computed positions: -k'1,$' */
 
 #if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
       && ('%' == 37) && ('&' == 38) && ('\'' == 39) && ('(' == 40) \
@@ -29,17 +29,16 @@
 #error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gnu-gperf@gnu.org>."
 #endif
 
-/* maximum key range = 24, duplicates = 0 */
+/* maximum key range = 35, duplicates = 0 */
 
 #ifndef GPERF_DOWNCASE
 #define GPERF_DOWNCASE 1
-static unsigned char gperf_downcase[256] =
-  {
-      0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
-     15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,
-     30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,
-     45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,
-     60,  61,  62,  63,  64,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106,
+static unsigned char gperf_downcase[256] = {
+    0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
+    15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,
+    30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,
+    45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,
+    60,  61,  62,  63,  64,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106,
     107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
     122,  91,  92,  93,  94,  95,  96,  97,  98,  99, 100, 101, 102, 103, 104,
     105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
@@ -53,26 +52,24 @@ static unsigned char gperf_downcase[256] =
     225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
     240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254,
     255
-  };
+};
 #endif
 
 #ifndef GPERF_CASE_STRNCMP
 #define GPERF_CASE_STRNCMP 1
 static int
-gperf_case_strncmp (register const char *s1, register const char *s2, register unsigned int n)
+gperf_case_strncmp (register const char* s1, register const char* s2, register unsigned int n)
 {
-  for (; n > 0;)
-    {
-      unsigned char c1 = gperf_downcase[(unsigned char)*s1++];
-      unsigned char c2 = gperf_downcase[(unsigned char)*s2++];
-      if (c1 != 0 && c1 == c2)
-        {
-          n--;
-          continue;
+    for (; n > 0;) {
+        unsigned char c1 = gperf_downcase[(unsigned char) * s1++];
+        unsigned char c2 = gperf_downcase[(unsigned char) * s2++];
+        if (c1 != 0 && c1 == c2) {
+            n--;
+            continue;
         }
-      return (int)c1 - (int)c2;
+        return (int)c1 - (int)c2;
     }
-  return 0;
+    return 0;
 }
 #endif
 
@@ -84,157 +81,166 @@ inline
 #endif
 #endif
 static unsigned int
-hash (register const char *str, register unsigned int len)
+hash (register const char* str, register unsigned int len)
 {
-  static const unsigned char asso_values[] =
-    {
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      22, 21, 19, 18, 16,  0, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25,  1, 25,  0, 25,
-       1,  0,  0, 13,  0, 25, 25, 11,  2,  1,
-       0, 25, 25,  5,  0,  2, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25,  1, 25,
-       0, 25,  1,  0,  0, 13,  0, 25, 25, 11,
-       2,  1,  0, 25, 25,  5,  0,  2, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-      25, 25, 25, 25, 25, 25, 25
+    static const unsigned char asso_values[] = {
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 16,
+        15, 13, 12, 11, 10, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 17, 18, 36,  8,  0,
+        0, 36, 17, 17, 36, 36,  8,  0,  3, 17,
+        0, 36, 10,  0,  3, 16,  6, 36, 36, 18,
+        36, 36, 36, 36, 36, 36, 36, 17, 18, 36,
+        8,  0,  0, 36, 17, 17, 36, 36,  8,  0,
+        3, 17,  0, 36, 10,  0,  3, 16,  6, 36,
+        36, 18, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36, 36, 36, 36, 36,
+        36, 36, 36, 36, 36, 36
     };
-  register int hval = (int)len;
-
-  switch (hval)
-    {
-      default:
-        hval += asso_values[(unsigned char)str[1]+1];
-      /*FALLTHROUGH*/
-      case 1:
-        hval += asso_values[(unsigned char)str[0]];
-        break;
-    }
-  return hval;
+    return len + asso_values[(unsigned char)str[len - 1]] + asso_values[(unsigned char)str[0]];
 }
 
-#ifdef __GNUC__
-__inline
-#ifdef __GNUC_STDC_INLINE__
-__attribute__ ((__gnu_inline__))
-#endif
-#endif
-const char *
-hoedown_find_block_tag (register const char *str, register unsigned int len)
+const char*
+hoedown_find_block_tag (register const char* str, register unsigned int len)
 {
-  enum
-    {
-      TOTAL_KEYWORDS = 24,
-      MIN_WORD_LENGTH = 1,
-      MAX_WORD_LENGTH = 10,
-      MIN_HASH_VALUE = 1,
-      MAX_HASH_VALUE = 24
+    enum {
+        TOTAL_KEYWORDS = 34,
+        MIN_WORD_LENGTH = 1,
+        MAX_WORD_LENGTH = 10,
+        MIN_HASH_VALUE = 1,
+        MAX_HASH_VALUE = 35
     };
 
-  if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
-    {
-      register int key = hash (str, len);
+    if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH) {
+        unsigned int key = hash (str, len);
 
-      if (key <= MAX_HASH_VALUE && key >= MIN_HASH_VALUE)
-        {
-          register const char *resword;
+        if (key <= MAX_HASH_VALUE && key >= MIN_HASH_VALUE) {
+            register const char* resword;
 
-          switch (key - 1)
-            {
-              case 0:
+            switch (key - 1) {
+            case 0:
                 resword = "p";
                 goto compare;
-              case 1:
-                resword = "h6";
-                goto compare;
-              case 2:
-                resword = "div";
-                goto compare;
-              case 3:
-                resword = "del";
-                goto compare;
-              case 4:
-                resword = "form";
-                goto compare;
-              case 5:
-                resword = "table";
-                goto compare;
-              case 6:
-                resword = "figure";
-                goto compare;
-              case 7:
+            case 2:
                 resword = "pre";
                 goto compare;
-              case 8:
-                resword = "fieldset";
+            case 3:
+                resword = "form";
                 goto compare;
-              case 9:
-                resword = "noscript";
-                goto compare;
-              case 10:
-                resword = "script";
-                goto compare;
-              case 11:
+            case 4:
                 resword = "style";
                 goto compare;
-              case 12:
+            case 5:
+                resword = "figure";
+                goto compare;
+            case 6:
+                resword = "main";
+                goto compare;
+            case 7:
+                resword = "table";
+                goto compare;
+            case 8:
+                resword = "script";
+                goto compare;
+            case 9:
+                resword = "section";
+                goto compare;
+            case 10:
+                resword = "fieldset";
+                goto compare;
+            case 11:
+                resword = "nav";
+                goto compare;
+            case 12:
+                resword = "figcaption";
+                goto compare;
+            case 13:
+                resword = "noscript";
+                goto compare;
+            case 14:
+                resword = "details";
+                goto compare;
+            case 15:
+                resword = "footer";
+                goto compare;
+            case 16:
+                resword = "div";
+                goto compare;
+            case 17:
                 resword = "dl";
                 goto compare;
-              case 13:
-                resword = "ol";
+            case 18:
+                resword = "del";
                 goto compare;
-              case 14:
-                resword = "ul";
-                goto compare;
-              case 15:
-                resword = "math";
-                goto compare;
-              case 16:
+            case 19:
                 resword = "ins";
                 goto compare;
-              case 17:
-                resword = "h5";
+            case 20:
+                resword = "math";
                 goto compare;
-              case 18:
+            case 21:
+                resword = "aside";
+                goto compare;
+            case 22:
                 resword = "iframe";
                 goto compare;
-              case 19:
-                resword = "h4";
+            case 23:
+                resword = "article";
                 goto compare;
-              case 20:
-                resword = "h3";
+            case 24:
+                resword = "summary";
                 goto compare;
-              case 21:
+            case 25:
+                resword = "ul";
+                goto compare;
+            case 26:
+                resword = "ol";
+                goto compare;
+            case 27:
                 resword = "blockquote";
                 goto compare;
-              case 22:
+            case 28:
+                resword = "h6";
+                goto compare;
+            case 29:
+                resword = "h5";
+                goto compare;
+            case 30:
+                resword = "h4";
+                goto compare;
+            case 31:
+                resword = "h3";
+                goto compare;
+            case 32:
+                resword = "header";
+                goto compare;
+            case 33:
                 resword = "h2";
                 goto compare;
-              case 23:
+            case 34:
                 resword = "h1";
                 goto compare;
             }
-          return 0;
-        compare:
-          if ((((unsigned char)*str ^ (unsigned char)*resword) & ~32) == 0 && !gperf_case_strncmp (str, resword, len) && resword[len] == '\0')
-            return resword;
+            return 0;
+compare:
+            if ((((unsigned char)*str ^ (unsigned char)*resword) & ~32) == 0 && !gperf_case_strncmp (str, resword, len)
+                && resword[len] == '\0')
+            { return resword; }
         }
     }
-  return 0;
+    return 0;
 }

3c887a99e5f7c8dcaba278538c76eae5e8f6d099
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2021-09-17 00:08:48 -0400

    remove unnecessary DEFAULT_LIBRARY_FUNCS_TO_INCLUDE
    it's empty by default

--- build.sh
+++ build.sh
@@ -16,7 +16,6 @@ emcc snudown.c src/autolink.c src/buffer.c src/markdown.c src/stack.c html/houdi
 -o build/snudown_emscripten.js \
 -Oz -flto=full --closure 2 -DNDEBUG \
 -s EXPORTED_FUNCTIONS=['_default_renderer','_wiki_renderer','_malloc','_free'] \
--s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE=[] \
 -s MALLOC=emmalloc \
 -s ABORTING_MALLOC=0 \
 -s ALLOW_MEMORY_GROWTH=1 \
